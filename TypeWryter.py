4f7d13fdfd233d5c1103cd1633c2e15c4d7b539d
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-31 22:13:20 -0500

    Update TypeWryter.py

--- TypeWryter.py
+++ TypeWryter.py
@@ -704,11 +704,13 @@ class TypeWryter:
             self.needs_diplay_update=False
             self.typing_last_time = time.time()
 
+        # If the user hasn't typed in over 30 seconds do a full clear and refresh of the screen.
         if (time.time()-self.typing_last_time) > (30):
             self.epd.init()
             self.epd.Clear()
             self.update_display()
             self.update_input_area()
+            self.needs_diplay_update=False
             keyboard.read_key()
             
         elif (time.time()-self.typing_last_time)<(.75): #if not doing a full refresh, do partials
